11e9cf9deb32f82ce3d8c79d18ec1e61c794e9df
duane-faria <duane.faria@etec.sp.gov.br>
2020-10-14 14:35:31 -0300

    Iniciando o projeto

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {}
+}
+
+module.exports = new App().express;
