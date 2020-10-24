e62a1d04d2d306cab4ae095536f7cde78c98a47e
duane-faria <duane.faria@etec.sp.gov.br>
2020-10-24 12:30:30 -0300

    Adicionando socket io a api

--- src/server.js
+++ src/server.js
@@ -1,3 +1,14 @@
 const app = require('./app');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.listen(process.env.PORT || 3000, () => console.log('server running'));
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.emit('message', 'Mensagem vinda da api');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    clearInterval(interval);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => console.log('server running'));
